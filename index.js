d959c5df421558df9d6f58e938d3992b0b0a1a10
Mats Andreassen <matsand@live.no>
2020-11-25 14:22:34 +0100

    Throw if an invalid date was passed

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 module.exports = dato => {
   const date = dato ? new Date(dato) : new Date()
+  if (!date || date.toString() === 'Invalid Date') throw new Error(`Invalid date: ${dato}`)
+
   const month = date.getMonth() + 1
   let thisYear = date.getFullYear()
 
